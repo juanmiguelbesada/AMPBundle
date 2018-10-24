3b21b945b91b9b76a11dce036993bf6371cb59fb
Juan Miguel Besada <juanmiguelbesada@gmail.com>
2018-10-24 10:52:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# JuanMiguelBesada/AMPBundle
---
+++ src/AMPBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace JuanMiguelBesada\AMPBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AMPBundle extends Bundle
+{
+
+}
